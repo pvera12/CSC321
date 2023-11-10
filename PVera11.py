4a74649bb6f1fe4ae664002c0a3539f80886b72a
pvera12 <pvera2@toromail.csudh>
2023-11-10 08:31:37 +0000

    Added PVera11.py

---
+++ PVera11.py
@@ -0,0 +1,17 @@
+# Priscilla Vera
+# 321-40
+# Lab 11
+
+user_input = input("Enter a number: ")
+
+number = float(user_input)
+
+result = number ** 2
+print(f"Your number squared is: {result}")
+
+
+saying = "The moment you give up, is the moment you let someone else win"
+print(saying)
+
+
+
